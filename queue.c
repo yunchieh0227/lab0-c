8335ff867dc510d801e7ddbb7d1062f404955f91
yunchieh <yunchieh0227@gmail.com>
2025-03-11 06:11:50 +0800

    Implement q_free to delete all elements
    Traverse and delete all elements before freeing the list and queue head.
    
    Change-Id: I9201f176e6dd8b6ae7a400fb9ae6f7dbd6591707

--- queue.c
+++ queue.c
@@ -15,7 +15,19 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    element_t *entry, *safe = NULL;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        list_del(&entry->list);
+        free(entry->value);
+        free(entry);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
