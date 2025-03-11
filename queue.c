2e26a174ef408a7943e6e53fd8f81a76fcc7fc95
yunchieh <yunchieh0227@gmail.com>
2025-03-11 16:32:13 +0800

    Implement q_remove_[head,tail] to remove elements
    Remove an element without freeing it, allowing further processing.
    
    Change-Id: I1377e48abda41c68509222c7f8b422e1ccb915ef

--- queue.c
+++ queue.c
@@ -72,14 +72,37 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *old_head = list_first_entry(head, element_t, list);
+    list_del(&old_head->list);
+
+    if (sp) {
+        strncpy(sp, old_head->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return old_head;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *old_tail = list_last_entry(head, element_t, list);
+    list_del(&old_tail->list);
+
+    if (sp) {
+        strncpy(sp, old_tail->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return old_tail;
 }
+
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
