79dbd380bffc845518ffd9ede962d703ab5963ad
yunchieh <yunchieh0227@gmail.com>
2025-03-11 17:18:50 +0800

    Implement q_size to count queue elements
    Traverse the queue to calculate the total number of elements.
    
    Change-Id: I5039db7bde50a79344ed54e17c1bc809943f9088

--- queue.c
+++ queue.c
@@ -107,7 +107,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int count = 0;
+    element_t *entry;
+    list_for_each_entry (entry, head, list)
+        count++;
+
+    return count;
 }
 
 /* Delete the middle node in queue */
